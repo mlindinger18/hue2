6982f6c40bd7323933ad2882f805fd26a023e14c
Marcel Lindinger <mlindinger18@gmail.com>
2020-09-28 14:35:44 +0200

    NumberTester edited

--- hue2/hue2/src/hue2/NumberTester.java
+++ hue2/hue2/src/hue2/NumberTester.java
@@ -11,18 +11,23 @@ package hue2;
  */
 public class NumberTester {
 
-    public String pfad;
+    private NumberTest primeTester;
+    private NumberTest oddTester;
+    private NumberTest palindromeTester;
 
     public NumberTester(String fileName) {
     }
 
     public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
     }
 
     public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
     }
 
     public void PalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
     }
 
     public void testFile() {
